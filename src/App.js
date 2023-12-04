0b1ef5b2e5a4e5e9548d58f9d898ffc6dc4c87c1
Adrian Dworak <adrian.dworak97@gmail.com>
2023-12-04 17:22:55 +0100

    filterPokemon

--- src/App.js
+++ src/App.js
@@ -12,6 +12,19 @@ function App() {
   const [pokemonList, setPokemonList] = useState([]); //Lista pokemonów
   const [allPokedex, setAllPokedex] = useState([]); //Szczególy każdego pokemona
 
+  const [allTypePokemon, setAllTypePokemon] = useState([...allPokedex]); //Szczególy każdego pokemona
+
+  const [allTypes, setAllTypes] = useState([
+    "all",
+    "fire",
+    "grass",
+    "water",
+    "bug",
+    "normal",
+  ]);
+
+  const [typeClick, setTypeClick] = useState("all");
+
   const [viewPokemon, setViewPokemon] = useState({});
 
   const [toggleClick, setToggleClick] = useState(false);
@@ -35,8 +48,30 @@ function App() {
     setToggleClick(true);
   };
 
-  const allPokemon = allPokedex.map((pokemon) => (
-    <PokemonComponent pokemon={pokemon} onClick={pokemonOnClick} />
+  const allPokemon =
+    typeClick === "all"
+      ? allPokedex.map((pokemon) => (
+          <PokemonComponent pokemon={pokemon} onClick={pokemonOnClick} />
+        ))
+      : allTypePokemon.map((pokemon) => (
+          <PokemonComponent pokemon={pokemon} onClick={pokemonOnClick} />
+        ));
+
+  const showTypes = allTypes.map((type) => (
+    <>
+      <li
+        onClick={() => {
+          setAllTypePokemon(
+            allPokedex.filter(
+              (pokemonType) => pokemonType.types[0].type.name === type
+            )
+          );
+          setTypeClick(type);
+        }}
+      >
+        {type}
+      </li>
+    </>
   ));
 
   return (
@@ -45,7 +80,10 @@ function App() {
         <Header />
       </div>
       <div id="main">
-        <div id="content">{allPokemon}</div>
+        <div id="content">
+          <div className="showTypes">{showTypes}</div>
+          <div className="showPokemon">{allPokemon}</div>
+        </div>
         <div id="aside">
           {toggleClick ? (
             <ViewPokemonComponent pokemonInfo={viewPokemon} />
